70a56ea205a3e93970eb4314f9ebc1923649fd9d
Greg Wang <gregwym@users.noreply.github.com>
2019-11-26 20:25:49 +0800

    factor out constants.

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as imagemin from 'imagemin';
 import imageminPngquant from 'imagemin-pngquant';
+import { PNGQUANT_OPTIONS } from './constants';
 
 export function getDestPath(source: string, dest: string, filePath: string) {
   return path.join(dest, path.relative(source, filePath));
@@ -9,10 +10,6 @@ export function getDestPath(source: string, dest: string, filePath: string) {
 export async function runImagemin(fromPath: string, toPath: string) {
   return imagemin([fromPath], {
     destination: toPath,
-    plugins: [
-      imageminPngquant({
-        speed: 1,
-      }),
-    ],
+    plugins: [imageminPngquant(PNGQUANT_OPTIONS)],
   });
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
+import { Options as PngquantOptions } from 'imagemin-pngquant';
+
 export const IMAGE_EXTENSIONS = new Set(['.png']);
+
+export const PNGQUANT_OPTIONS: PngquantOptions = {
+  speed: 1,
+};
