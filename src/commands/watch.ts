a391deb45da598c2c40337e3c27310d7431b21bd
Greg Wang <gregwym@users.noreply.github.com>
2019-11-28 14:32:44 +0800

    Factor out compress flow to common.

--- src/commands/watch.ts
+++ src/commands/watch.ts
@@ -1,95 +1,17 @@
-import * as fs from 'fs';
 import watch from 'node-watch';
-import * as path from 'path';
 
-import { getDestPath, logFileEvent, runImagemin, safeFsStat, shouldIgnore } from '../common';
-import { IGNORED_FILE_SIZE } from '../constants';
-import { TaskScheduler } from '../scheduler';
+import { compressToDest, removeDestFile, shouldIgnore } from '../common';
 import { WatchCmdOptions } from '../types';
 
-interface CompressRequest {
-  sourcePath: string;
-  destPath: string;
-  sourceStats: fs.Stats;
-}
-
 export function startWatch(source: string, dest: string, options: WatchCmdOptions = {}) {
-  const taskScheduler = new TaskScheduler<CompressRequest>(async (requests: CompressRequest[]) => {
-    await Promise.all(
-      requests.map(async ({ sourcePath, destPath, sourceStats }) => {
-        try {
-          await runImagemin(sourcePath, path.dirname(destPath));
-
-          const destStats = await safeFsStat(destPath);
-
-          logFileEvent('compressed', {
-            from: sourcePath,
-            to: destPath,
-            origin: sourceStats.size,
-            output: destStats?.size,
-          });
-          return destStats;
-        } catch (e) {
-          logFileEvent('compress-failed', {
-            from: sourcePath,
-            to: destPath,
-            error: e,
-          });
-          return;
-        }
-      }),
-    );
-    return [];
-  });
-
-  async function onChange(filePath: string) {
-    const destPath = getDestPath(source, dest, filePath);
-    const fileStat = await safeFsStat(filePath);
-    if (!fileStat) {
-      logFileEvent('not-found', { from: filePath });
-      return;
-    }
-
-    if (fileStat.size < IGNORED_FILE_SIZE) {
-      logFileEvent('skip-small', { from: filePath });
-    }
-
-    const destStat = await safeFsStat(destPath);
-    if (destStat && destStat.mtime >= fileStat.mtime) {
-      logFileEvent('not-modified', { from: filePath, to: destPath, modifiedAt: destStat.mtime });
-    }
-
-    logFileEvent('modified', { from: filePath, to: destPath, modifiedAt: fileStat.mtime });
-    taskScheduler.push({
-      sourcePath: filePath,
-      destPath: destPath,
-      sourceStats: fileStat,
-    });
-  }
-
-  async function onRemove(filePath: string) {
-    const destPath = getDestPath(source, dest, filePath);
-    logFileEvent('deleted', { from: filePath, to: destPath });
-
-    // Make sure have write permission
-    try {
-      await fs.promises.access(destPath, fs.constants.F_OK | fs.constants.W_OK);
-    } catch (e) {
-      logFileEvent('skip-delete', { from: filePath, to: destPath });
-      return;
-    }
-
-    await fs.promises.unlink(destPath);
-  }
-
   console.info(`Start to watch ${source} and will output to ${dest}.`);
 
   const watcher = watch(source, { recursive: true, filter: p => !shouldIgnore(p) }, async (fileEvent, filePath) => {
     try {
       if (fileEvent === 'update') {
-        await onChange(filePath);
+        await compressToDest(source, dest, filePath);
       } else if (fileEvent === 'remove') {
-        await onRemove(filePath);
+        await removeDestFile(source, dest, filePath);
       }
     } catch (e) {
       console.error('Handler error: ', e);
--- src/common.ts
+++ src/common.ts
@@ -6,7 +6,8 @@ import imageminPngquant from 'imagemin-pngquant';
 import * as path from 'path';
 import * as prettyBytes from 'pretty-bytes';
 
-import { IGNORED_PATHS, IMAGE_EXTENSIONS, MOZJPEG_OPTIONS, PNGQUANT_OPTIONS } from './constants';
+import { IGNORED_FILE_SIZE, IGNORED_PATHS, IMAGE_EXTENSIONS, MOZJPEG_OPTIONS, PNGQUANT_OPTIONS } from './constants';
+import { TaskScheduler } from './scheduler';
 
 export function shouldIgnore(filePath: string) {
   return anymatch(IGNORED_PATHS, filePath) || !IMAGE_EXTENSIONS.has(path.extname(filePath).toLowerCase());
@@ -62,4 +63,78 @@ export async function safeFsStat(p: string) {
     if (err.code === 'ENOENT') return null;
     throw err;
   });
+}
+
+interface CompressRequest {
+  sourcePath: string;
+  destPath: string;
+  sourceStats: fs.Stats;
+}
+
+const taskScheduler = new TaskScheduler<CompressRequest>(async (requests: CompressRequest[]) => {
+  await Promise.all(
+    requests.map(async ({ sourcePath, destPath, sourceStats }) => {
+      try {
+        await runImagemin(sourcePath, path.dirname(destPath));
+
+        const destStats = await safeFsStat(destPath);
+
+        logFileEvent('compressed', {
+          from: sourcePath,
+          to: destPath,
+          origin: sourceStats.size,
+          output: destStats?.size,
+        });
+        return destStats;
+      } catch (e) {
+        logFileEvent('compress-failed', {
+          from: sourcePath,
+          to: destPath,
+          error: e,
+        });
+        return;
+      }
+    }),
+  );
+  return [];
+});
+
+export async function compressToDest(source: string, dest: string, filePath: string) {
+  const destPath = getDestPath(source, dest, filePath);
+  const fileStat = await safeFsStat(filePath);
+  if (!fileStat) {
+    logFileEvent('not-found', { from: filePath });
+    return;
+  }
+
+  if (fileStat.size < IGNORED_FILE_SIZE) {
+    logFileEvent('skip-small', { from: filePath });
+  }
+
+  const destStat = await safeFsStat(destPath);
+  if (destStat && destStat.mtime >= fileStat.mtime) {
+    logFileEvent('not-modified', { from: filePath, to: destPath, modifiedAt: destStat.mtime });
+  }
+
+  logFileEvent('modified', { from: filePath, to: destPath, modifiedAt: fileStat.mtime });
+  taskScheduler.push({
+    sourcePath: filePath,
+    destPath: destPath,
+    sourceStats: fileStat,
+  });
+}
+
+export async function removeDestFile(source: string, dest: string, filePath: string) {
+  const destPath = getDestPath(source, dest, filePath);
+  logFileEvent('deleted', { from: filePath, to: destPath });
+
+  // Make sure have write permission
+  try {
+    await fs.promises.access(destPath, fs.constants.F_OK | fs.constants.W_OK);
+  } catch (e) {
+    logFileEvent('skip-delete', { from: filePath, to: destPath });
+    return;
+  }
+
+  await fs.promises.unlink(destPath);
 }
